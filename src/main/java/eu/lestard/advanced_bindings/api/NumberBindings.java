1172f94d0975f23a85bf88adda5daf93c7861731
Manuel Mauky <manuel.mauky@googlemail.com>
2014-08-30 21:29:11 +0200

    Added Binding for safe division

--- src/main/java/eu/lestard/advanced_bindings/api/NumberBindings.java
+++ src/main/java/eu/lestard/advanced_bindings/api/NumberBindings.java
@@ -2,7 +2,13 @@ package eu.lestard.advanced_bindings.api;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.IntegerBinding;
+import javafx.beans.binding.NumberBinding;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ObservableDoubleValue;
+import javafx.beans.value.ObservableIntegerValue;
+import javafx.beans.value.ObservableNumberValue;
 
 public class NumberBindings {
 
@@ -14,4 +20,35 @@ public class NumberBindings {
         return Bindings.createBooleanBinding(() -> Double.isInfinite(observableValue.get()), observableValue);
     }
 
+    public static NumberBinding divideSafe(ObservableNumberValue dividend, ObservableNumberValue divisor) {
+        return divideSafe(dividend, divisor, new SimpleDoubleProperty(0));
+    }
+
+    public static NumberBinding divideSafe(ObservableNumberValue dividend, ObservableNumberValue divisor, ObservableNumberValue defaultValue) {
+        return Bindings.createDoubleBinding(() -> {
+
+            if (divisor.doubleValue() == 0) {
+                return defaultValue.doubleValue();
+            } else {
+                return dividend.doubleValue() / divisor.doubleValue();
+            }
+
+        }, dividend, divisor);
+    }
+
+    public static IntegerBinding divideSafe(ObservableIntegerValue dividend, ObservableIntegerValue divisor){
+        return divideSafe(dividend, divisor, new SimpleIntegerProperty(0));
+    }
+
+    public static IntegerBinding divideSafe(ObservableIntegerValue dividend, ObservableIntegerValue divisor, ObservableIntegerValue defaultValue){
+        return Bindings.createIntegerBinding(() -> {
+
+            if (divisor.intValue() == 0) {
+                return defaultValue.get();
+            } else {
+                return dividend.intValue() / divisor.intValue();
+            }
+
+        }, dividend, divisor);
+    }
 }
--- src/test/java/eu/lestard/advanced_bindings/api/NumberBindingsTest.java
+++ src/test/java/eu/lestard/advanced_bindings/api/NumberBindingsTest.java
@@ -2,8 +2,12 @@ package eu.lestard.advanced_bindings.api;
 
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.DoubleBinding;
+import javafx.beans.binding.IntegerBinding;
+import javafx.beans.binding.NumberBinding;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import org.junit.Test;
 
 import static eu.lestard.assertj.javafx.api.Assertions.*;
@@ -50,5 +54,101 @@ public class NumberBindingsTest {
 
         assertThat(infinite).isTrue();
     }
+
+
+    @Test
+    public void testDivideSafeDouble(){
+        DoubleProperty a = new SimpleDoubleProperty();
+        DoubleProperty b = new SimpleDoubleProperty();
+
+        final NumberBinding quotient = NumberBindings.divideSafe(a, b);
+
+        assertThat(quotient).hasValue(0.0);
+
+        a.set(10);
+        assertThat(quotient).hasValue(0.0);
+
+        b.set(5);
+        assertThat(quotient).hasValue(2.0);
+
+        a.set(12);
+        assertThat(quotient).hasValue(2.4);
+
+        b.set(0);
+        assertThat(quotient).hasValue(0.0);
+    }
+
+    @Test
+    public void testDivideSafeDoubleWithDefaultValue(){
+        DoubleProperty a = new SimpleDoubleProperty();
+        DoubleProperty b = new SimpleDoubleProperty();
+
+        DoubleProperty defaultValue = new SimpleDoubleProperty(3.2);
+
+        final NumberBinding quotient = NumberBindings.divideSafe(a, b, defaultValue);
+
+        assertThat(quotient).hasSameValue(defaultValue);
+        assertThat(quotient).hasSameValue(defaultValue);
+
+        a.set(10);
+        assertThat(quotient).hasSameValue(defaultValue);
+
+        b.set(5);
+        assertThat(quotient).hasValue(2.0);
+
+        a.set(12);
+        assertThat(quotient).hasValue(2.4);
+
+        b.set(0);
+        assertThat(quotient).hasSameValue(defaultValue);
+    }
+
+    @Test
+    public void testDivideSafeInteger(){
+
+        IntegerProperty a = new SimpleIntegerProperty();
+        IntegerProperty b = new SimpleIntegerProperty();
+
+        final IntegerBinding quotient = NumberBindings.divideSafe(a, b);
+
+        assertThat(quotient).hasValue(0);
+
+        a.set(10);
+        assertThat(quotient).hasValue(0);
+
+        b.set(5);
+        assertThat(quotient).hasValue(2);
+
+        a.set(12);
+        assertThat(quotient).hasValue(2);
+
+        b.set(0);
+        assertThat(quotient).hasValue(0);
+    }
+
+    @Test
+    public void testDivideSafeIntegerWithDefaultValue(){
+        IntegerProperty a = new SimpleIntegerProperty();
+        IntegerProperty b = new SimpleIntegerProperty();
+
+        IntegerProperty defaultValue = new SimpleIntegerProperty(10);
+
+        final IntegerBinding quotient = NumberBindings.divideSafe(a, b, defaultValue);
+
+        assertThat(quotient).hasSameValue(defaultValue);
+
+        a.set(10);
+        assertThat(quotient).hasSameValue(defaultValue);
+
+        b.set(5);
+        assertThat(quotient).hasValue(2);
+
+        a.set(12);
+        assertThat(quotient).hasValue(2);
+
+        b.set(0);
+        assertThat(quotient).hasSameValue(defaultValue);
+    }
+
 
 }
