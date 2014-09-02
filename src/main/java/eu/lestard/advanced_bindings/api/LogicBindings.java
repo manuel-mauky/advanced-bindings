85d0ecef9d503e5f8dadb1b0e168bd4ea2aa3462
Manuel Mauky <manuel.mauky@googlemail.com>
2014-09-02 20:14:34 +0200

    Add javadoc for logic bindings

--- src/main/java/eu/lestard/advanced_bindings/api/LogicBindings.java
+++ src/main/java/eu/lestard/advanced_bindings/api/LogicBindings.java
@@ -9,6 +9,17 @@ import java.util.Arrays;
 
 public class LogicBindings {
 
+    /**
+     * A boolean binding that is <code>true</code> only when all dependent observable boolean values
+     * are <code>true</code>.
+     *
+     * This can be useful in cases where the
+     * {@link Bindings#and(javafx.beans.value.ObservableBooleanValue, javafx.beans.value.ObservableBooleanValue)}
+     * with 2 arguments isn't enough.
+     *
+     * @param values variable number of observable boolean values that are used for the binding
+     * @return the boolean binding
+     */
     public static BooleanBinding and(ObservableBooleanValue...values) {
         return Bindings.createBooleanBinding(
             ()-> !Arrays.stream(values)
@@ -17,6 +28,17 @@ public class LogicBindings {
                 .isPresent(), values);
     }
 
+    /**
+     * A boolean binding that is only <code>true</code> when at leased one of the dependent observable boolean values
+     * are <code>true</code>.
+     *
+     * This can be useful in cases where the
+     * {@link Bindings#or(javafx.beans.value.ObservableBooleanValue, javafx.beans.value.ObservableBooleanValue)}
+     * with 2 arguments isn't enough.
+     *
+     * @param values variable number of observable boolean values that are used for the binding
+     * @return the boolean binding
+     */
     public static BooleanBinding or(ObservableBooleanValue...values) {
         return Bindings.createBooleanBinding(()-> Arrays.stream(values)
             .filter(ObservableBooleanValue::get)
