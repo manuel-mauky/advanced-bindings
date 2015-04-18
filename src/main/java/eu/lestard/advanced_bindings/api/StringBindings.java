11bc67ffd4e64ea414bd6962d3edb870c48fdd5b
Manuel Mauky <manuel.mauky@googlemail.com>
2015-04-18 17:30:40 +0200

    Add string bindings for trim, toLowerCase and toUpperCase

--- src/main/java/eu/lestard/advanced_bindings/api/StringBindings.java
+++ src/main/java/eu/lestard/advanced_bindings/api/StringBindings.java
@@ -2,43 +2,179 @@ package eu.lestard.advanced_bindings.api;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.StringBinding;
 import javafx.beans.value.ObservableValue;
+
+import java.util.Locale;
 
 /**
  * This class contains custom binding implementations for Strings.
  */
 public class StringBindings {
+	
+	
+	/**
+	 * Creates a boolean binding that is `true` when the given observable string matches the given RegExp pattern.
+	 *
+	 * @param text
+	 *            the observable string that is verified.
+	 * @param pattern
+	 *            the RegExp pattern that is used.
+	 * @return a boolean binding instance.
+	 */
+	public static BooleanBinding matches(final ObservableValue<String> text, final String pattern) {
+		return Bindings.createBooleanBinding(() -> {
+			final String textToVerify = text.getValue();
+			return textToVerify != null && textToVerify.matches(pattern);
+		}, text);
+	}
+	
+	/**
+	 * Creates a boolean binding that is `true` when the given observable string matches the given RegExp pattern (also
+	 * given as observable string).
+	 *
+	 * @param text
+	 *            the observable string that is verified.
+	 * @param pattern
+	 *            the observable string with the RegExp pattern that is used.
+	 * @return a boolean binding instance.
+	 */
+	public static BooleanBinding matches(final ObservableValue<String> text, final ObservableValue<String> pattern) {
+		return Bindings.createBooleanBinding(() -> {
+			final String textToVerify = text.getValue();
+			final String patternString = pattern.getValue();
+			
+			return textToVerify != null && patternString != null && textToVerify.matches(patternString);
+		}, text, pattern);
+	}
+	
+	/**
+	 * Creates a string binding that contains the trimmed value of the given observable string. See
+	 * {@link String#trim()}.
+	 *
+	 * If the given observable string has a value of `null` the created binding will contain an empty string.
+	 *
+	 * @param text
+	 *            the source string that will used for trimming.
+	 * @return a binding containing the trimmed string.
+	 */
+	public static StringBinding trim(ObservableValue<String> text) {
+		return Bindings.createStringBinding(()
+				-> text.getValue() == null ? "" : text.getValue().trim(), text);
+	}
+	
+	
+	/**
+	 * Creates a string binding that contains the value of the given observable string converted to lowercase. See
+	 * {@link String#toLowerCase()}.
+	 *
+	 * If the given observable string has a value of `null` the created binding will contain an empty string.
+	 * 
+	 * @param text
+	 *            the source string that will used for the conversion.
+	 * @return a binding containing the lowercase string.
+	 */
+	public static StringBinding toLowerCase(ObservableValue<String> text) {
+		return Bindings.createStringBinding(()
+				-> text.getValue() == null ? "" : text.getValue().toLowerCase(), text);
+	}
+	
+	/**
+	 * Creates a string binding that contains the value of the given observable string converted to lowercase with the
+	 * given locale. See {@link String#toLowerCase(Locale)}.
+	 *
+	 * If the given observable string has a value of `null` the created binding will contain an empty string.
+	 *
+	 * @param text
+	 *            the source string that will used for the conversion.
+	 * @param locale
+	 *            the locale that will be used for conversion.
+	 * @return a binding containing the lowercase string.
+	 */
+	public static StringBinding toLowerCase(ObservableValue<String> text, Locale locale) {
+		return Bindings.createStringBinding(()
+				-> text.getValue() == null ? "" : text.getValue().toLowerCase(locale), text);
+	}
 
 
     /**
-     * Creates a boolean binding that is `true` when the given observable string matches the given
-     * RegExp pattern.
+     * Creates a string binding that contains the value of the given observable string converted to lowercase with the
+     * given locale. See {@link String#toLowerCase(Locale)}.
      *
-     * @param text the observable string that is verified.
-     * @param pattern the RegExp pattern that is used.
-     * @return a boolean binding instance.
+     * If the given observable string has a value of `null` the created binding will contain an empty string.
+     *
+     * If the given observable locale has a value of `null` {@link Locale#getDefault()} will be used instead.
+     *
+     * @param text
+     *            the source string that will used for the conversion.
+     * @param locale
+     *            an observable containing the locale that will be used for conversion.
+     * @return a binding containing the lowercase string.
      */
-    public static BooleanBinding matches(final ObservableValue<String> text, final String pattern) {
-        return Bindings.createBooleanBinding(()->{
-            final String textToVerify = text.getValue();
-            return textToVerify != null && textToVerify.matches(pattern);
-        }, text);
-    }
+	public static StringBinding toLowerCase(ObservableValue<String> text, ObservableValue<Locale> locale) {
+		return Bindings.createStringBinding(() -> {
+			if (text.getValue() == null) {
+				return "";
+			}
+			
+			final Locale localeValue = locale.getValue() == null ? Locale.getDefault() : locale.getValue();
+			
+			return text.getValue().toLowerCase(localeValue);
+		}, text, locale);
+	}
 
     /**
-     * Creates a boolean binding that is `true` when the given observable string matches the given
-     * RegExp pattern (also given as observable string).
+     * Creates a string binding that contains the value of the given observable string converted to uppercase. See
+     * {@link String#toUpperCase()}.
      *
-     * @param text the observable string that is verified.
-     * @param pattern the observable string with the RegExp pattern that is used.
-     * @return a boolean binding instance.
+     * If the given observable string has a value of `null` the created binding will contain an empty string.
+     *
+     * @param text
+     *            the source string that will used for the conversion.
+     * @return a binding containing the uppercase string.
      */
-    public static BooleanBinding matches(final ObservableValue<String> text, final ObservableValue<String> pattern) {
-        return Bindings.createBooleanBinding(()->{
-            final String textToVerify = text.getValue();
-            final String patternString = pattern.getValue();
+	public static StringBinding toUpperCase(ObservableValue<String> text) {
+		return Bindings.createStringBinding(()
+				-> text.getValue() == null ? "" : text.getValue().toUpperCase(), text);
+	}
 
-            return textToVerify != null && patternString != null && textToVerify.matches(patternString);
-        }, text, pattern);
-    }
+    /**
+     * Creates a string binding that contains the value of the given observable string converted to uppercase with the
+     * given locale. See {@link String#toUpperCase(Locale)}.
+     *
+     * If the given observable string has a value of `null` the created binding will contain an empty string.
+     *
+     * @param text
+     *            the source string that will used for the conversion.
+     * @param locale
+     *            the locale that will be used for conversion.
+     * @return a binding containing the uppercase string.
+     */
+	public static StringBinding toUpperCase(ObservableValue<String> text, Locale locale) {
+		return Bindings.createStringBinding(()
+				-> text.getValue() == null ? "" : text.getValue().toUpperCase(locale), text);
+	}
+
+
+    /**
+     * Creates a string binding that contains the value of the given observable string converted to uppercase with the
+     * given locale. See {@link String#toUpperCase(Locale)}.
+     *
+     * If the given observable string has a value of `null` the created binding will contain an empty string.
+     *
+     * If the given observable locale has a value of `null` {@link Locale#getDefault()} will be used instead.
+     *
+     * @param text
+     *            the source string that will used for the conversion.
+     * @param locale
+     *            an observable containing the locale that will be used for conversion.
+     * @return a binding containing the uppercase string.
+     */
+	public static StringBinding toUpperCase(ObservableValue<String> text, ObservableValue<Locale> locale) {
+		return Bindings.createStringBinding(() -> {
+			final Locale localeValue = locale.getValue() == null ? Locale.getDefault() : locale.getValue();
+			
+			return text.getValue() == null ? "" : text.getValue().toUpperCase(localeValue);
+		}, text, locale);
+	}
 }
--- src/test/java/eu/lestard/advanced_bindings/api/StringBindingsTest.java
+++ src/test/java/eu/lestard/advanced_bindings/api/StringBindingsTest.java
@@ -1,9 +1,14 @@
 package eu.lestard.advanced_bindings.api;
 
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import org.junit.Test;
+
+import java.util.Locale;
 
 import static eu.lestard.assertj.javafx.api.Assertions.assertThat;
 
@@ -75,4 +80,113 @@ public class StringBindingsTest {
         pattern.set(null);
         assertThat(matches).isFalse();
     }
+
+
+    @Test
+    public void testTrim() {
+        StringProperty text = new SimpleStringProperty();
+
+        StringBinding trimmed = StringBindings.trim(text);
+        assertThat(trimmed).hasValue("");
+
+        text.setValue("test");
+        assertThat(trimmed).hasValue("test");
+
+        text.setValue("test  ");
+        assertThat(trimmed).hasValue("test");
+
+        text.setValue("   test  ");
+        assertThat(trimmed).hasValue("test");
+    }
+
+    @Test
+    public void testToLowercase () {
+        StringProperty text = new SimpleStringProperty();
+
+        StringBinding lowercase = StringBindings.toLowerCase(text);
+
+        assertThat(lowercase).hasValue("");
+
+        text.setValue("Test");
+
+        assertThat(lowercase).hasValue("test");
+    }
+
+    @Test
+    public void testToLowercaseWithLocale () {
+        StringProperty text = new SimpleStringProperty();
+
+        StringBinding lowercase = StringBindings.toLowerCase(text, Locale.ENGLISH);
+
+        assertThat(lowercase).hasValue("");
+
+        text.setValue("Test");
+
+        assertThat(lowercase).hasValue("test");
+
+    }
+
+    @Test
+    public void testToLowercaseWithObservableLocale(){
+        StringProperty text = new SimpleStringProperty();
+        ObjectProperty<Locale> locale = new SimpleObjectProperty<>();
+
+        final StringBinding lowercase = StringBindings.toLowerCase(text, locale);
+
+        assertThat(lowercase).hasValue("");
+
+
+        text.setValue("Test İ"); // 'İ' will be converted to 'i' in turkish language
+
+        assertThat(lowercase).hasValue("test i̇");
+
+        locale.setValue(Locale.forLanguageTag("tr"));
+        assertThat(lowercase).hasValue("test i");
+    }
+
+
+    @Test
+    public void testToUppercase () {
+        StringProperty text = new SimpleStringProperty();
+
+        StringBinding uppercase = StringBindings.toUpperCase(text);
+
+        assertThat(uppercase).hasValue("");
+
+        text.setValue("Test");
+
+        assertThat(uppercase).hasValue("TEST");
+    }
+
+    @Test
+    public void testToUppercaseWithLocale () {
+        StringProperty text = new SimpleStringProperty();
+
+        StringBinding uppercase = StringBindings.toUpperCase(text, Locale.ENGLISH);
+
+        assertThat(uppercase).hasValue("");
+
+        text.setValue("Test");
+
+        assertThat(uppercase).hasValue("TEST");
+    }
+
+    @Test
+    public void testToUppercaseWithObservableLocale(){
+        StringProperty text = new SimpleStringProperty();
+        ObjectProperty<Locale> locale = new SimpleObjectProperty<>();
+
+        final StringBinding uppercase = StringBindings.toUpperCase(text, locale);
+
+        assertThat(uppercase).hasValue("");
+
+
+        text.setValue("Test i");
+
+        assertThat(uppercase).hasValue("TEST I");
+
+        locale.setValue(Locale.forLanguageTag("tr"));
+        assertThat(uppercase).hasValue("TEST İ");
+    }
+
 }
